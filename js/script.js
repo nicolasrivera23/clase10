32bc777bc488d2c227aa48d49f6f26c01544434b
nicolasrivera23 <113543987+nicolasrivera23@users.noreply.github.com>
2025-10-30 21:08:00 -0300

    primer commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+//Condicionales
+
+//if
+/*
+if (edad >= 18) {
+  console.log("Sos mayor de edad.");
+}
+
+//if else
+if (edad >= 18) {
+  console.log("Sos mayor de edad.");
+} else {
+  console.log("Sos menor de edad.");
+}
+
+
+let edad = 12;
+
+// if - else if - else
+if (edad >= 18) {
+  console.log("Sos mayor de edad.");
+} else if (edad >= 13) {
+  console.log("Sos un adolescente.");
+} else {
+  console.log("Sos un niño/a.");
+}  
+*/
+
+//Operador Ternario
+let edad = 18;
+let resultado = edad >= 18 ? "mayor de edad" : "menor de edad";
+console.log("Estado Edad: " + resultado);
